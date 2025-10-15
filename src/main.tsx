44818668d788b5bead70a126cc4f1d00b6cde7b3
Radios02 <119680276+Radios02@users.noreply.github.com>
2025-10-15 11:31:53 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 import PizzaPage from "./pages/PizzaPage.tsx";
-import 404 from "./pages/404.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,7 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path='/' element={<App />}/>
         <Route path='/pizzak/:id' element={<PizzaPage />}/>
-        <Route path='/404' element={<404 />}/>
+        <Route path='*' elent={<h1>404, az oldal nem található</h1>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
